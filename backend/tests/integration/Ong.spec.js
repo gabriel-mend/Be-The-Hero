5c092e2be245f08e6a4c4187b58b89a77a7a9671
System Administrator <root@Gabriel-Mac-mini.local>
2020-03-29 10:39:11 -0300

    Funcionalidades avançadas

---
+++ backend/tests/integration/Ong.spec.js
@@ -0,0 +1,26 @@
+const request = require('supertest')
+const app = require('../../src/app')
+const connection = require('../../src/database/connection')
+
+describe('ONG', () => {
+    beforeEach(async() => {
+        await connection.migrate.rollback()
+        await connection.migrate.latest()
+    })
+    afterAll(async() => {
+        await connection.destroy()
+    })
+    it('Should be able to create a new ONG', async () => {
+        const response = await request(app)
+            .post('/ongs')
+            .send({
+                name: "apad2",
+                email: "contato@ajnsdda.com",
+                whatsapp: "1234567891",
+                city: "maranhao",
+                uf: "MA"
+            })
+        expect(response.body).toHaveProperty('id')
+        expect(response.body.id).toHaveLength(8)
+    })
+})
